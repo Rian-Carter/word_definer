74e9cc017359aa58c5088f6650938df37bc26c93
Rian Carter <R.Carter86@hotmail.com>
2022-05-06 09:33:28 -0700

    add first test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,3 +1,11 @@
 require('rspec')
 require('word')
 require('define')
+
+describe ('Word') do
+  describe('.all') do
+    it("returns an empty array when there are no albums") do
+      expect(Word.all).to(eq([]))
+    end
+  end
+end
