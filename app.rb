37ebb94aad67c39703288f8afa76d3cf6ed7070d
Rian Carter <R.Carter86@hotmail.com>
2022-05-06 11:48:06 -0700

    add route for deleting a word

--- app.rb
+++ app.rb
@@ -43,3 +43,10 @@ patch('/words/:id') do
   @words = Word.all
   erb(:words)
 end
+
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
+end
