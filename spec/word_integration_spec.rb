ebfb2fb5614e8e2bf8e171885f5c904973769f6d
Rian Carter <R.Carter86@hotmail.com>
2022-05-06 15:06:33 -0700

    incorporate automated testing using Capybara

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,3 +3,23 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
+describe('create a word path', {:type => :feature}) do
+  it('creates a word and then goes to the word page') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'In Rainbows')
+    click_on('Go!')
+    expect(page).to have_content('In Rainbows')
+  end
+end
+
+describe('create a definition path', {:type => :feature}) do
+  it('creates a word and then goes to the word page') do
+    word = Word.new("In Rainbows", nil)
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('definition_name', :with => 'Bodysnatchers')
+    click_on('Add definition')
+    expect(page).to have_content('Bodysnatchers')
+  end
+end
