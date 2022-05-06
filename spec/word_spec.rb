e0fa2ed9a7c9753b9d9601470d1fe64e38bd27b6
Rian Carter <R.Carter86@hotmail.com>
2022-05-06 10:28:30 -0700

    add test for delete method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -61,4 +61,15 @@ describe ('Word') do
     end
   end
 
+  describe('#delete') do
+    it("deletes a word by id") do
+      word = Word.new("In Rainbows", nil)
+      word.save()
+      word2 = Word.new("Yellow", nil)
+      word2.save()
+      word.delete()
+      expect(Word.all).to(eq([word2]))
+    end
+  end
+
 end
