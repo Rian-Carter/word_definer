1ebb1d5a896ac7d1283a3498a62988415415659e
Rian Carter <R.Carter86@hotmail.com>
2022-05-08 09:01:42 -0700

    add 4 new tests. Edit a word, edit a definition, delete a word, and delete a definition

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -23,3 +23,51 @@ describe('create a definition path', {:type => :feature}) do
     expect(page).to have_content('Bodysnatchers')
   end
 end
+
+describe('edit a word', {:type => :feature}) do
+  it("edits a word and returns to word page") do
+    word = Word.new("Roundabout", nil)
+    word.save()
+    visit("/words/#{word.id}")
+    click_on("Edit Word")
+    fill_in("Change Word", :with => "Paradise")
+    click_on("Update")
+    expect(page).to have_content("Paradise")
+  end
+end
+
+describe('edit a definition', {:type => :feature}) do
+  it("edits a words definition and returns to word page") do
+    word = Word.new("Sailboat", nil)
+    word.save()
+    definition = Definition.new("wind powered boat", word.id, nil)
+    definition.save()
+    visit("/words/#{word.id}/definitions/#{definition.id}")
+    fill_in("Definition", :with => "boat powered by wind")
+    click_on("Update")
+    expect(page).to have_content("boat powered by wind")
+  end
+end
+
+describe('delete a word', {:type => :feature}) do
+  it("deletes a word and returns to word page") do
+    word = Word.new("Sailboat", nil)
+    word.save()
+    visit("/words/#{word.id}")
+    click_on("Edit Word")
+    click_on("Delete Word")
+    expect(page).to have_content("")
+  end
+end
+
+describe("deletes a definition", { :type => :feature }) do
+  it("delete a definition and return to word page") do
+    word = Word.new("Sailboat", nil)
+    word.save()
+    definition = Definition.new("wind powered boat", word.id, nil)
+    definition.save()
+    visit("/words/#{word.id}/definitions/#{definition.id}")
+    click_on("Delete")
+    expect(page).to(have_content(""))
+  end
+end
